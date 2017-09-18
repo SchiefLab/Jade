b3deb7063948dad570465a12684842389fdde854
Jared Adolf-Bryfogle <jadolfbr@gmail.com>
2017-09-18 16:42:06 -0500

    new app fixes.

---
+++ apps/strip_ANISOU.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from argparse import ArgumentParser
+import re
+
+parser = ArgumentParser(" Strips ANISOU lines out of PDBs.")
+
+parser.add_argument("pdb_files", help = "Path to PDB file we will be stripping.", nargs="*")
+
+
+options = parser.parse_args()
+
+for pdb_file in options.pdb_files:
+    FILE = open(pdb_file, 'r')
+    lines = FILE.readlines()
+    FILE.close()
+
+
+
+    OUTFILE = open(pdb_file, 'w')
+    for line in lines:
+        if not re.search("ANISOU", line):
+            OUTFILE.write(line)
+    OUTFILE.close()
