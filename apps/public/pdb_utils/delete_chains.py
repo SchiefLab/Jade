19e1f3474cd40839a05d528372b5bcee14410158
Jared Adolf-Bryfogle <jadolfbr@gmail.com>
2018-04-27 11:50:32 -0500

    update delete chains app.

--- apps/public/pdb_utils/delete_chains.py
+++ apps/public/pdb_utils/delete_chains.py
@@ -8,6 +8,13 @@ from argparse import ArgumentParser
 import os
 from jade.basic.path import open_file, get_decoy_name
 
+
+def conditional_keep_lines(keep_chains, delete_chains, current_chain, outlines):
+    if (len(keep_chains) > 0) and current_chain in keep_chains:
+        outlines.append(line)
+    elif (len(delete_chains) > 0) and current_chain not in delete_chains:
+        outlines.append(line)
+
 if __name__ == "__main__":
 
     parser = ArgumentParser("Delete chains from a PDB (not cif) file. Only works with ATOM records.  ")
@@ -17,6 +24,8 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--chains", help = "Comma-separated list of chains to remove")
 
     parser.add_argument("-k", "--keep_chains", help = "Chains to keep.  Use this instead of the chains option")
+
+    parser.add_argument("--suffix", help = "Suffix to add to output file", default = "_del")
 
     options = parser.parse_args()
 
@@ -37,29 +46,44 @@ if __name__ == "__main__":
         linked_chain = ""
         if line.startswith("ATOM"):
             current_chain = line[21]
-            print(line)
-            print(current_chain)
+            #print(line)
+            #print(current_chain)
 
-            if (len(keep_chains) > 0) and current_chain in keep_chains:
-                outlines.append(line)
-                continue
-            if (len(delete_chains) > 0 ) and current_chain in delete_chains:
-                continue
+            conditional_keep_lines(keep_chains, delete_chains, current_chain, outlines)
+            continue
 
-        if line.startswith("LINK"):
+        elif line.startswith("LINK"):
             current_chain = line[21]
             linked_chain = line[51]
 
-            if (len(keep_chains) > 0) and (current_chain in keep_chains) and (linked_chain in keep_chains):
-                outlines.append(line)
-                continue
-            if (len(delete_chains) > 0 ) and ((current_chain in delete_chains) or (linked_chain in delete_chains)):
-                continue
+            conditional_keep_lines(keep_chains, delete_chains, current_chain, outlines)
+            conditional_keep_lines(keep_chains, delete_chains, linked_chain, outlines)
+            continue
+
+        elif line.startswith("HETATM"):
+            current_chain = line[21]
+            conditional_keep_lines(keep_chains, delete_chains, current_chain, outlines)
+            continue
+
+        elif line.startswith("TER"):
+            current_chain = line[21]
+            conditional_keep_lines(keep_chains, delete_chains, current_chain, outlines)
+            continue
+        elif line.startswith("SSBOND"):
+            current_chain = line[15]
+            linked_chain = line[29]
+
+            conditional_keep_lines(keep_chains, delete_chains, current_chain, outlines)
+            conditional_keep_lines(keep_chains, delete_chains, linked_chain, outlines)
+            continue
+
+        else:
+            outlines.append(line)
 
     INFILE.close()
-    OUTFILE = open(get_decoy_name(options.s)+"_del.pdb", 'w')
+    OUTFILE = open(get_decoy_name(options.s)+options.suffix+".pdb", 'w')
     OUTFILE.writelines(outlines)
-    print("done")
+    print("Done")
 
 
 
